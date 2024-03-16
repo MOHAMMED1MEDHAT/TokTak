27954fd71b68fd68a02d5a24e2d938f165901612
mohammed medhat <mohamed.medhat2121@gmail.com>
2024-03-16 15:36:46 +0200

    feat(auth):add user presistance layer

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { ConfigsModule } from './config/config.module';
+import { typeOrmConfig } from './config/typeorm.config';
 import { UserModule } from './user/user.module';
 
 @Module({
-	imports: [AuthModule, UserModule],
+	imports: [
+		AuthModule,
+		UserModule,
+		ConfigsModule,
+		TypeOrmModule.forRoot(typeOrmConfig),
+	],
 })
 export class AppModule {}
